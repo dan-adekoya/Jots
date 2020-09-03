814cfd8bc2a19406029bd66d1e2fcd9ab10aacf0
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-09-03 01:09:43 +0100

    init

---
+++ src/components/Developer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Dev</h1>
+        <router-link class="router-link" to="/">Jots</router-link>
+        <router-link class="router-link" to="/manage">Manage</router-link>
+        <router-link class="router-link" to="/dev">Dev</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+        
+    }
+},
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import Jots from './components/Jots.vue'
+import Manage from './components/Manage.vue'
+import Dev from './components/Developer.vue'
+
+
+
+
+
+let router = new VueRouter({
+  routes : [
+    {path: '/', component: Jots},
+    {path: '/manage', component: Manage},
+    {path: '/dev', component: Dev}
+  ],
+  mode: 'history'
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
