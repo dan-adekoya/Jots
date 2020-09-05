88c1bee893ce312a7122912782e2265cb036b318
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-09-05 13:15:55 +0100

    jots-page-ui-implemtation-#3

--- src/components/Jots.vue
+++ src/components/Jots.vue
@@ -6,9 +6,15 @@
       <div class="btn">
         <button class="mainBtn">Save</button>
         <button class="mainBtn">Clear</button>
-        <button disabled="disabled" class="Btn">Preview Note</button>
+        <p disabled="disabled" class="Btn" @click="open =! open">Preview Note</p>
       </div>
     </form>
+    <div class="preview" v-bind:class="{open: open}">
+      <img src="../assets/images/close.svg" alt="" class="close" @click="open =! open">
+        <h1>Preview</h1>
+        <h2>Title</h2>
+        <p></p>
+    </div>
   </div>
 </template>
 
@@ -16,7 +22,7 @@
 export default {
   data() {
     return {
-      
+      open: false
     }
   },
 }
@@ -62,13 +68,14 @@ form textarea{
 .Btn{
   flex: 3;
 }
-.btn button{
+.btn button, p{
   margin: 0 2%; 
   font-size: 20px;
   padding: 20px 40px;
   border: none;
   outline: none;
-  color: var(--white)
+  color: var(--white);
+  cursor: pointer;
 }
 .btn button:nth-child(1){
   background: #0DFF6E
@@ -76,11 +83,61 @@ form textarea{
 .btn button:nth-child(2){
   background: #FF4444
 }
-.btn button:nth-child(3){
+.btn p{
+  padding: 20px  0;
   background: #6D7CFF
 }
+/* PREVIEW */
+.preview{
+  position: fixed;
+  top: 0;
+  left: 0;
+  visibility: hidden;
+  color: var(--white);
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.616);
+}
+.preview.open{
+  visibility: visible;
+}
+.close{
+  position: absolute;
+  top: 20px;
+  width: 30px;
+  right: 20px;
+  cursor: pointer;
+}
+.preview h1{
+  transform: translateY(30px);
+  width: 200px;
+  margin: auto;
+}
+.preview h2{
+  width: 90%;
+  padding: 10px;
+  margin: 0 auto;
+  margin-top: 40px;
+  background: #3A4774;
+}
+.preview p{
+  width: 90%;
+  height: 70vh;
+  overflow: hidden;
+  overflow-y: scroll;
+  margin: 0 auto;
+  margin-top: 40px;
+  background: #3A4774;
+  padding: 10px;
+}
+.preview p::-webkit-scrollbar{
+  width: 6px;
+}
+.preview p::-webkit-scrollbar-thumb{
+  background: var(--secondary)
+}
 @media screen and (max-width: 900px) {
- .btn button{
+ .btn button, p{
    margin: 0 0%;
    padding: 20px 20px;
    font-size: 15px;
